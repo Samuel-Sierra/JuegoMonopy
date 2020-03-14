22060cbeeb0807cf26a9a686838bab2d8d3d734f
Samuel-Sierra <61210632+Samuel-Sierra@users.noreply.github.com>
2020-03-14 17:11:53 -0600

    comit3

--- js/generar.js
+++ js/generar.js
@@ -8,3 +8,33 @@ window.requestAnimFrame =( function () {
             window.setTimeout(callback, 1000 / 60);
         };
 })();
+arrayRemove = function (array, from) {
+    var rest = array.slice((from) + 1 || array.length);
+    array.length = from < 0 ? array.length + from : from;
+    return array.push.apply(array, rest);
+};
+
+var game=(function(){
+	//aqui van a venir las variable
+
+	var posicion,
+		carasDado,
+		totalBestScoresToShow,
+		player,
+		dadoImages={
+			animation : [],
+			revuelto : new Image()
+		},
+		playerImage,
+		juegoImage,
+		miniJuego1Image,
+		miniJuego2Image,
+		miniJuego3Image,
+		miniJuego4Image,
+		miniJuego5Image,
+		miniJuego6Image,
+		inicioFinImage;
+
+	function preLoadImages() {
+		// body...
+		juegoImage = new Image();
