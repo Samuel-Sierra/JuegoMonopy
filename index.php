ef2048bc9d197b6b406455f71bc89e458d49c880
Samuel-Sierra <bocalin@hotmail.com>
2020-03-14 13:56:38 -0600

    comit1

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+	<title>Monopoly</title>
+</head>
+<body>
+	<script type="text/javascript" src="generar.js"></script>
+	<div id="contenedor">
+		<div id="arriba">Aqui va una cabecera de opciones por si acaso</div>
+		<div id="abajo">Aqui deberia de ir el juego</div>
+	</div>
+</body>
+</html>
